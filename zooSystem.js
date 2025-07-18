333127f521e38b82579ece3535709bf221294336
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2025-07-17 18:00:24 -0700

    Added Dog class that extends from Animal, updated makeSound method to exemplify polymorphism

--- zooSystem.js
+++ zooSystem.js
@@ -1,14 +1,48 @@
 // Zoo Management System
 
 // Define an animal class
-class Animal{
-    #name
-    #age
-    constructor(name, age) {
-        this.#name = name;
-        this.#age = age;
-        console.log(`Created a new animal: ${this.#name}! Current Age: ${this.#age}`)
-    }
+class Animal {
+  #name;
+  #age;
+  constructor(name, age) {
+    this.#name = name;
+    this.#age = age;
+    console.log(`Created a new animal: ${this.#name}!`);
+  }
+
+  // Common methods for Animal (encapsulated, reusable)
+  getInfo() {
+    return `${this.#name} is ${this.#age} years old.`;
+  }
+
+  eat() {
+    console.log(`${this.#name} is eating.`);
+  }
+
+  makeSound() {
+    console.log(`${this.#name} makes a generic sound`);
+  }
 }
 
-const tiger = new Animal("Tiger", 3)
+class Dog extends Animal {
+  constructor(name, age) {
+    super(name, age);
+    this.breed = "Labrador";
+  }
+  // Logs barking sound
+  makeSound() {
+    console.log(`${this.getInfo().split(" is")[0]} barks: Woof!`);
+  }
+}
+
+// Test Animal Object and Methods
+
+const tiger = new Animal("Tiger", 5);
+tiger.eat();
+console.log(tiger.getInfo());
+
+// Test New Extended Dog Class
+const Buddy = new Dog("Buddy", 4);
+Buddy.makeSound();
+Buddy.eat();
+console.log(Buddy.getInfo());
